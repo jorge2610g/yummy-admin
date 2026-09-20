aecfe68a939dcfa7044c5426f3fe17edac3868a8
jorge2610g <scuentas150@gmail.com>
2026-09-20 11:30:25 -0300

    Remove monitoring module

--- monitor/playwright.production.config.js
+++
@@ -1,2 +0,0 @@
-const {defineConfig}=require('@playwright/test');
-module.exports=defineConfig({testDir:'.',testMatch:'production.spec.js',timeout:40000,retries:1,workers:1,use:{trace:'retain-on-failure',screenshot:'only-on-failure'},reporter:[['line'],['html',{outputFolder:'production-report',open:'never'}]]});
